e0714bb42d7614d7628e0ac0c1db55b8d1510c00
lkb0398 <lkb0398@nate.com>
2021-10-14 22:27:34 -0400

    initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,18 @@
+/* 
+FileName: app.css
+Name: kyeongbeom Lee
+StudentNumber: 301083034 
+Date: Sep 30, 2021.
+*/
+
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+ 
+    function Start(){
+ 
+        console.log("App started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET Index page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express'});
+});
+
+/* GET Home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Home'});
+});
+
+/* GET About us page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About'});
+});
+
+/* GET Projects page. */
+router.get('/project', function(req, res, next) {
+  res.render('project', { title: 'Projects'});
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services'});
+});
+
+/* GET Contact us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact us'});
+});
+
+module.exports = router;
